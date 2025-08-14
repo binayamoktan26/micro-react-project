e7627620d712c56ede5af6bc5f7e395fec0840da
nawang tashi lama <binayamoktan26@gmail.com>
2025-08-14 18:30:22 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="wrapper"
+      style={{
+        display: "flex",
+        height: "100vh",
+        fontSize: "1.2rem",
+      }}
+    >
+      <div
+        className="user-list"
+        style={{
+          boxShadow: "0 0 10px black",
+          padding: "2rem",
+        }}
+      >
+        <div className="display">as you type</div>
+        <div className="form">
+          <form action="">
+            <input type="text" />
+            <button>Add User</button>
+          </form>
+          <hr />
+          <div className="list">
+            <ul>
+              <li>Kakashi Hatake </li>
+              <li>Itachi Uchiha </li>
+              <li>Shikamaru Nara </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
